fb5035b7295db698bfd2d4d94916c6c9069be9b8
FradexMF <friveror@uc.cl>
2026-07-28 19:06:19 -0400

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/example/task/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.task
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/jvmMain/kotlin/com/example/task/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.example.task
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
